b7140875b7bdf5ac6848cde99ed0f3451734c0f2
ephyrass <jb.houi@yahoo.fr>
2021-01-15 14:12:39 +0100

    v0.2

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -42,24 +42,27 @@ const Portfolio = () => {
   ];
 
   return (
-    <div className='portfolio'>
-      {items.map((item) => (
-        <PortfolioItem item={item} />
-      ))}
-      <div>
-        <svg
-          className='wave'
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 1440 320'
-        >
-          <path
-            fill='#d3b929'
-            fill-opacity='1'
-            d='M0,96L60,122.7C120,149,240,203,360,224C480,245,600,235,720,197.3C840,160,960,96,1080,74.7C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z'
-          ></path>
-        </svg>
+    <>
+      <div className='portfolio'>
+        <h2 className='about-title'>Nos Réalisation</h2>
+        {items.map((item) => (
+          <PortfolioItem item={item} />
+        ))}
+        <div>
+          <svg
+            className='wave'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 1440 320'
+          >
+            <path
+              fill='#d3b929'
+              fill-opacity='1'
+              d='M0,96L60,122.7C120,149,240,203,360,224C480,245,600,235,720,197.3C840,160,960,96,1080,74.7C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z'
+            ></path>
+          </svg>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,50 +1,58 @@
-import React from "react"
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 function PortfolioItem({ item }) {
   return (
     <>
-      <div className="portfolio-item">
-        <div className={item.way === 0 ? "pair-left" : "pair-right"}>
-          <figure
-            className="image-responsive"
-            style={
-              item.way === 0
-                ? {
-                    background: `url(${item.image2})`,
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                  }
-                : {
-                    background: `url(${item.image1})`,
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                  }
-            }
-          ></figure>
+      <ScrollAnimation
+        delay={500}
+        animateIn={item.way === 0 ? "fadeInLeft" : "fadeInRight"}
+        duration='1'
+        animateOnce={true}
+      >
+        <div className='portfolio-item'>
+          <div className={item.way === 0 ? "pair-left" : "pair-right"}>
+            <figure
+              className='image-responsive'
+              style={
+                item.way === 0
+                  ? {
+                      background: `url(${item.image2})`,
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                    }
+                  : {
+                      background: `url(${item.image1})`,
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                    }
+              }
+            ></figure>{" "}
+          </div>
+          <div className='middle'>
+            <p className='title-portfolio'>{item.title}</p>
+            <p className='subtitle'>{item.subTitle}</p>
+          </div>
+          <div className={item.way === 0 ? "pair-right" : "pair-left"}>
+            <figure
+              className='image-monitor'
+              style={
+                item.way === 0
+                  ? {
+                      background: `url(${item.image1})`,
+                      backgroundSize: "cover",
+                    }
+                  : {
+                      background: `url(${item.image2})`,
+                      backgroundSize: "cover",
+                    }
+              }
+            ></figure>
+          </div>
         </div>
-        <div className="middle">
-          <p className="title-portfolio">{item.title}</p>
-          <p className="subtitle">{item.subTitle}</p>
-        </div>
-        <div className={item.way === 0 ? "pair-right" : "pair-left"}>
-          <figure
-            className="image-monitor"
-            style={
-              item.way === 0
-                ? {
-                    background: `url(${item.image1})`,
-                    backgroundSize: "cover",
-                  }
-                : {
-                    background: `url(${item.image2})`,
-                    backgroundSize: "cover",
-                  }
-            }
-          ></figure>
-        </div>
-      </div>
+      </ScrollAnimation>
     </>
-  )
+  );
 }
 
-export default PortfolioItem
+export default PortfolioItem;
